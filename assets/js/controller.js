d7b1c1598a3d5c527777349b5118f1ef7796ac3e
Jordan Long <jordan.long222@gmail.com>
2016-08-12 16:38:15 -0400

    add content, styling updates

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,15 +1,3 @@
-// $(function(){
-//   var $window = $(window);
-//   var $scrollUp = $('#scrollUp');
-//   var endZone = $('#portfolio').offset().top - $window.height() - 500;
-//
-//   console.log('hey');
-//
-//   $window.on('scroll', function(){
-//     if ( (endZone) < $window.scrollTop() ) {
-//       $scrollUp.animate({ 'right': '0px'}, 250);
-//     } else {
-//       $scrollUp.stop(true).animate({ 'right': '-360px'}, 250);
-//     }
-//   });
-// });
+app.controller('navController',function(){
+  this.navBar = false;
+});
